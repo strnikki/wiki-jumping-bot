d72462c56f02d1918a4899193ef17d6874d746e2
Nicole Fanelli <nicolefanelli@protonmail.com>
2020-12-07 18:44:29 -0300

    Write basic unit tests

---
+++ scraper.py
@@ -0,0 +1,22 @@
+import requests
+from requests.exceptions import HTTPError
+
+def main():
+    pass
+
+def get_page_data():
+    pass
+
+def get_title():
+    pass
+
+def get_links():
+    pass
+
+def get_list_of_titles(url, n_steps):
+    pass
+    # titles = []
+    # for i in range(n_steps):
+    #     page_body = get_page_data(url).text
+    #     titles.append(get_title(page_body))
+    #     titles
---
+++ test_scraper.py
@@ -0,0 +1,19 @@
+import scraper
+import pytest
+from requests.exceptions import HTTPError
+
+def test_can_connect():
+    assert (scraper.get_page_data().status_code == 200)
+
+def test_fail_to_connect():
+    with pytest.raises(HTTPError):
+        assert scraper.get_page_data()
+
+def test_can_return_title():
+    assert (scraper.get_title() == "Title of Page")
+
+def test_can_return_list_of_links():
+    assert (type(scraper.get_links()) is list)
+
+def test_can_return_list_of_titles():
+    assert False
